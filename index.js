0ee48ea150b46fc269bb55df53e2f53c421bc00d
Gagandeep <gag7550@gmail.com>
2020-07-19 08:51:15 +0530

    initial

--- index.js
+++ index.js
@@ -27,11 +27,17 @@ connection.once("open", () => {
 
 // API
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("./frontend/build"));
-}
-
 app.use("/api", router);
+
+if (
+  process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "staging"
+) {
+  app.use(express.static("frontend/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
+  });
+}
 
 // listen port
 app.listen(port, () => {
